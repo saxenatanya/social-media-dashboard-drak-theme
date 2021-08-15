c987bc2c584b36f4e302e5effc6ac50c1934b0aa
Tanya Saxena <82470912+saxenatanya@users.noreply.github.com>
2021-08-15 13:55:25 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
+
 let checkBox = document.querySelector('#switch');
+let head = document.querySelector('.head-section');
+let creator = document.querySelectorAll('.creator-card');
+let bottom = document.querySelectorAll('.bottom__card');
+
+
 console.log(checkBox);
 checkBox.addEventListener('change', function () {
     if (this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light');
-        console.log(this.checked);
+        console.log(creator);
+        document.body.setAttribute('data-theme', 'light');
+        head.setAttribute('data-theme', 'light');
+        creator.setAttribute('data-theme', 'light');
+        bottom.setAttribute('data-theme', 'light');
+       
     } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.body.setAttribute('data-theme', 'dark');
+        head.setAttribute('data-theme', 'dark');
+        creator.setAttribute('data-theme', 'dark');
+        bottom.setAttribute('data-theme', 'dark');
     }
 });
 
-let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition')
-    },1000);
-}
+// let trans = () => {
+//     document.documentElement.classList.add('transition');
+//     window.setTimeout(() => {
+//         document.documentElement.classList.remove('transition')
+//     },1000);
+// }
